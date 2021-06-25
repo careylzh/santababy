f4289b826ed0378c85b7f32e036e768588d77c37
Carey <carey@glints.com>
2021-06-25 13:18:14 +0800

    Housekeeping: refined notes for dangerfile.ts

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,6 +1,8 @@
 /**This file contains mandatory Prefixes for commit messages to ensure commit consistency.
  * To be implemented with github CI/CD if there's time
- * Using setup for all boilerplate code, Verify and Redeem for specific changes in business logic after setup is done
+ * Using 'Setup' for all boilerplate code,
+ * 'Verify' and 'Redeem' for specific changes in business logic after setup is done
+ * Housekeeping commits could be reordered and squashed accordingly if there's time
  */
 
 const Rules = ["Setup", "Housekeeping", "Verify", "Redeem"];
