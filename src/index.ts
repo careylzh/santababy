e4db61c2076e65536a7f0210375935101ef7ec34
Carey <carey@glints.com>
2021-06-27 00:21:46 +0800

    Setup: added Frontend and included in express server index.ts
    Also added cors package just in case of future cross-origin deployments

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@ import morgan from "morgan";
 
 import Router from "./routes";
 import dbConfig from "./config/database";
-
+import cors from "cors";
 const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
